46be9045ada4cd3626b24273adac730ff7b6a1e0
irxground <irxnjhtchlnrw@gmail.com>
2013-05-17 17:50:31 +0900

    Use JRuby

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import javax.script._
+
 
 object Application extends Controller {
-  
+  val rubycode = """
+
+def fib(n)
+  if n <= 1
+    1
+  else
+    fib(n-1) + fib(n-2)
+  end
+end
+
+"fib 10 = #{fib(10)}"
+  """
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val m = new ScriptEngineManager()
+    val e = m.getEngineByName("jruby")
+    val c = e.getContext()
+
+    var hello = e.eval(rubycode, c)
+
+    val txt = "Your new application is ready." + hello
+    Ok(views.html.index(txt))
   }
-  
 }
--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "org.jruby" % "jruby-core" % "1.7.4"
   )
 
 
